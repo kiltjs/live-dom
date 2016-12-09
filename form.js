1cdf104a2541b73466e6f18e91796d024204de2b
Jesús Germade <jesus@germade.es>
2016-12-09 19:51:51 +0100

    widget and form supports umd

--- form.js
+++ form.js
@@ -1,25 +1,43 @@
 
 // forms
 
-var $live = require('./live-selector'),
-	formRunning = null,
-	formHandlers = {};
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd && typeof require === 'function') {
+        // AMD. Register as an anonymous module.
+        require(['$live'], function ($live) {
+          factory($live);
+        });
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('./live-selector'));
+    } else {
+        // Browser globals (root is window)
+        factory(root.$live);
+  }
+})(this, function ($live) {
 
-$live.form = function (formName, handler) {
-	formHandlers[formName] = handler;
+  var formRunning = null,
+      formHandlers = {};
 
-	if( formRunning === null ) {
+  $live.form = function (formName, handler) {
+    formHandlers[formName] = handler;
 
-	  formRunning = false;
-	  $live('form[name]', function () {
-	    if( formHandlers[this.name] ) {
-	      formHandlers[this.name].apply(this, arguments);
-	    }
-	  }, function () {
-	    formRunning = true;
-	  });
+    if( formRunning === null ) {
 
-	} else if( formRunning ) {
-	  [].forEach.call(document.querySelectorAll('form[name="' + formName + '"]'), handler);
-	}
-};
+      formRunning = false;
+      $live('form[name]', function () {
+        if( formHandlers[this.name] ) {
+          formHandlers[this.name].apply(this, arguments);
+        }
+      }, function () {
+        formRunning = true;
+      });
+
+    } else if( formRunning ) {
+      [].forEach.call(document.querySelectorAll('form[name="' + formName + '"]'), handler);
+    }
+  };
+
+});
--- widget.js
+++ widget.js
@@ -1,27 +1,43 @@
 
 // widgets
 
-var $live = require('./live-selector'),
-    widgetRunning = null,
-    widgetHandlers = {};
-
-$live.widget = function (widgetName, handler) {
-  widgetHandlers[widgetName] = handler;
-
-  if( widgetRunning === null ) {
-
-    widgetRunning = false;
-    $live('[data-widget]', function () {
-      if( widgetHandlers[this.getAttribute('data-widget')] ) {
-        widgetHandlers[this.getAttribute('data-widget')].apply(this, arguments);
-      }
-    }, function () {
-      widgetRunning = true;
-    });
-
-  } else if( widgetRunning ) {
-    [].forEach.call(document.querySelectorAll('[data-widget="' + widgetName + '"]'), handler);
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd && typeof require === 'function') {
+        // AMD. Register as an anonymous module.
+        require(['$live'], function ($live) {
+          factory($live);
+        });
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('./live-selector'));
+    } else {
+        // Browser globals (root is window)
+        factory(root.$live);
   }
-};
+})(this, function ($live) {
 
-module.exports = $live.widget;
+  var widgetRunning = null,
+      widgetHandlers = {};
+
+  $live.widget = function (widgetName, handler) {
+    widgetHandlers[widgetName] = handler;
+
+    if( widgetRunning === null ) {
+
+      widgetRunning = false;
+      $live('[data-widget]', function () {
+        if( widgetHandlers[this.getAttribute('data-widget')] ) {
+          widgetHandlers[this.getAttribute('data-widget')].apply(this, arguments);
+        }
+      }, function () {
+        widgetRunning = true;
+      });
+
+    } else if( widgetRunning ) {
+      [].forEach.call(document.querySelectorAll('[data-widget="' + widgetName + '"]'), handler);
+    }
+  };
+
+});
